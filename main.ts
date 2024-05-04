39431ebcfbe044fa29de82ced4aaa67ed5a596ad
ms0ur <maksimsuslov7142@gmail.com>
2024-05-04 15:59:52 +0300

    first commit

---
+++ main.ts
@@ -0,0 +1,12 @@
+import { createApp, createRouter, defineEventHandler } from "h3";
+
+export const app = createApp();
+
+const router = createRouter();
+app.use(router);
+
+router.get("/",
+    defineEventHandler((event) => {
+        return {message: "Hello World"}
+    })
+)
